4c722336f089839a7b5bd1ae6b04a17a6ad69a98
sohankyatham <Sohan.Kyatham23@gmail.com>
2026-03-24 19:00:55 -0400

    placeholder function to developing recommendation system based on score

---
+++ analysis/scoring.py
@@ -0,0 +1,7 @@
+def score_hour(row):
+    """
+    Score a single hour of conditions from 0-100.
+    row is one row of the weather DataFrame.
+    """
+    # We'll build this together in the next session
+    pass
